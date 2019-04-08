b93c8b5ea9854a16598e35a1c857e52cf5bf2e4d
Quentin FOURNIER <xeheros@gmail.com>
2019-04-08 09:33:07 +0200

    Remove debug logs

--- index.js
+++ index.js
@@ -17,33 +17,41 @@ client.on('message', msg => {
         if(text.substr(0, 1) === '!')
         {
             var regexp = /[^\s"']+|"([^"]*)"|'([^']*)'/g;
-            var cmd = text.substr(1).match(regexp);
-            var args = cmd.splice(1);
+            var args = text.substr(1).match(regexp);
+            var params = args.splice(1);
+            var cmd = args[0];
 
             for(var i = 0 ; i < args.length ; i++)
             {
-                args[i] = args[i].replace(/\"/g, "")
-                console.log("args[" + i + "]: " + args[i]);
+                params[i] = params[i].replace(/\"/g, "")
+                console.log("args[" + i + "]: " + params[i]);
             }
 
             switch(cmd)
             {
                 case 'r':
-                    var item = args[0];
+                    var item = params[0];
                     try
                     {
-                        var quantity = parseInt(args[1]);
-                        if(args.length == 2 && !isNaN(quantity))
+                        var quantity = parseInt(params[1]);
+                        if(params.length == 2 && !isNaN(quantity))
                         {
-                            console.log("OK");
                             // Notify creation of the request by direct message.
                             var requestsChannel = msg.guild.channels.find(channel => channel.id === config.requestChannel);
-                            requestsChannel.send(msg.member.displayName + ' vient de créer une requête pour ' + quantity + " x " + item + ' !');
+                            requestsChannel.send(msg.member.displayName + ' a besoin de ' + quantity + " x " + item + '.').then(
+                                function (botMsg)
+                                {
+                                    botMsg.react(":white_check_mark");
+                                }
+                            ).catch(function()
+                            {
+                                console.log("L'annonce n'a pas pu être envoyée !");
+                            });
+
                         }
                         else
                         {
-                            console.log("KO");
-                            msg.member.send('Vous avez envoyé trop d\'arguments (' + args.length + '), 2 étaient attendus (objet, quantité).');
+                            msg.member.send('Vous avez envoyé trop d\'arguments (' + params.length + '), 2 étaient attendus (objet, quantité).');
                         }
                     }
                     catch(error)
